3d32e1d2c6118ecfe23992be0ae675e8cfb506f0
lopezrendell143 <lopezrendell143@gmail.com>
2026-07-14 14:06:05 +0800

    commited new

--- myProject/routes/web.php
+++ myProject/routes/web.php
@@ -1,17 +1,26 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Everything inside this group is protected by Laravel Breeze authentication
+Route::middleware(['auth', 'verified'])->group(function () {
+    
+    // Change this line to load our Student Controller on your main dashboard
+    Route::get('/dashboard', [StudentController::class, 'index'])->name('dashboard');
+    
+    // Core CRUD action pathways
+    Route::post('/students', [StudentController::class, 'store'])->name('students.store');
+    Route::get('/students/{student}/edit', [StudentController::class, 'edit'])->name('students.edit');
+    Route::put('/students/{student}', [StudentController::class, 'update'])->name('students.update');
+    Route::delete('/students/{student}', [StudentController::class, 'destroy'])->name('students.destroy');
 
-Route::middleware('auth')->group(function () {
+    // Default Breeze profile actions (Do not delete)
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
