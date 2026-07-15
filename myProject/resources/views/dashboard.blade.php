e5c53489838846707f4c4f2b8acf3b9068f4ab00
lopezrendell143 <lopezrendell143@gmail.com>
2026-07-15 10:50:50 +0800

    okay na ulit

--- myProject/resources/views/dashboard.blade.php
+++ myProject/resources/views/dashboard.blade.php
@@ -59,16 +59,15 @@
                                                 View Info
                                             </button>
 
-                                            <button 
+                                            <x-primary-button 
                                                 @click="activeStudent = {{ json_encode($student) }}; openEditModal = true" 
                                                 type="button"
-                                                class="inline-flex items-center px-3 py-1.5 bg-indigo-50 dark:bg-indigo-900/30 text-indigo-600 dark:text-indigo-400 hover:bg-indigo-100 dark:hover:bg-indigo-900/50 rounded-md transition-colors"
                                             >
                                                 <svg class="w-4 h-4 mr-1.5" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                                                 </svg>
                                                 Edit
-                                            </button>
+                                            </x-primary-button>
 
                                             <form action="{{ route('students.destroy', $student->id) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this student record?');">
                                                 @csrf
