3556b9c2f8a259591ccae52d61f884573a3c7aeb
lopezrendell143 <lopezrendell143@gmail.com>
2026-07-15 09:25:01 +0800

    gg sana maayos na

--- myProject/resources/views/auth/register.blade.php
+++ myProject/resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{ config('app.name', 'Campus SIS') }} | Create Account</title>
+        <title>{{ config('app.name', 'Campus SIS') }} | Create Your Account</title>
 
         @fonts
 
